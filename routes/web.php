5986828154bf9b852d40d14b05faac725b3d432f
venelinmandov <venelinmandov98@gmail.com>
2022-03-15 19:11:54 +0200

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Auth::routes();
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index']);
 });
+
+
